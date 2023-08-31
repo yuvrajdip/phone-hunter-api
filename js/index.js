670e26d00d93df02c73f8c5544008f0d67a4e4e9
yuvrajdip <dipbhowmik2018@gmail.com>
2023-08-31 16:30:50 +0600

    first commit

---
+++ js/index.js
@@ -0,0 +1,10 @@
+
+console.log('js');
+
+const loadPhone = async() => {
+  const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
+  const data = await res.json()
+  const phones = data.data
+  console.log(phones)
+}
+loadPhone();
